a475842a83f2da0f93dea6a501731db4ce2d9f5e
Hushkisses <97074606+Hushkisses@users.noreply.github.com>
2026-09-25 14:38:29 +0900

    DEV-006 update status command

--- paper-plugin/src/main/java/com/hushkisses/spacesurvival/paper/SpaceCommand.java
+++ paper-plugin/src/main/java/com/hushkisses/spacesurvival/paper/SpaceCommand.java
@@ -41,6 +41,6 @@ final class SpaceCommand implements CommandExecutor {
                         + "§7~§f"
                         + plugin.configuration().game().maxPlayers()
         );
-        sender.sendMessage("§7현재 DEV: §eDEV-005 Sector/Room/Connection Model");
+        sender.sendMessage("§7현재 DEV: §eDEV-006 Tile Registration");
     }
 }
