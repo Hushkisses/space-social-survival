22569325667b37b9647b535c906f01aa87e1b249
Hushkisses <97074606+Hushkisses@users.noreply.github.com>
2026-09-25 14:28:21 +0900

    DEV-004 implement configuration system

---
+++ paper-plugin/src/main/java/com/hushkisses/spacesurvival/paper/config/PluginConfiguration.java
@@ -0,0 +1,10 @@
+package com.hushkisses.spacesurvival.paper.config;
+
+import com.hushkisses.spacesurvival.config.BalanceConfig;
+import com.hushkisses.spacesurvival.config.GameConfig;
+
+public record PluginConfiguration(
+        GameConfig game,
+        BalanceConfig balance
+) {
+}
