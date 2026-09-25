1fe19c6c2f6cbe1d0fa47736aaedf9f8c11a39ac
Hushkisses <97074606+Hushkisses@users.noreply.github.com>
2026-09-25 16:52:42 +0900

    DEV-020 add resource types and definitions

---
+++ core/src/main/java/com/hushkisses/spacesurvival/resource/ResourceType.java
@@ -0,0 +1,11 @@
+package com.hushkisses.spacesurvival.resource;
+
+public enum ResourceType {
+    REPAIR_PARTS,
+    CIRCUITS,
+    POWER_CELLS,
+    FUEL,
+    MEDICAL_SUPPLIES,
+    BIO_SAMPLES,
+    DATA_CORES
+}
