ce387da7e54814a816215a54b1883c00bffee939
Hushkisses <97074606+Hushkisses@users.noreply.github.com>
2026-09-25 20:46:28 +0900

    DEV-032 add meeting session model

---
+++ core/src/main/java/com/hushkisses/spacesurvival/social/meeting/MeetingStartDenialReason.java
@@ -0,0 +1,9 @@
+package com.hushkisses.spacesurvival.social.meeting;
+
+public enum MeetingStartDenialReason {
+    MEETING_ALREADY_ACTIVE,
+    BRIDGE_UNAVAILABLE,
+    NO_POWER_OR_COMMUNICATION,
+    COOLDOWN_ACTIVE,
+    NO_PARTICIPANTS
+}
