88ed25ed3f2ed81c64bfe4ba3c5fbfc1600034f1
Hushkisses <97074606+Hushkisses@users.noreply.github.com>
2026-09-25 16:49:42 +0900

    DEV-017 add facility action access and engineering diagnosis

---
+++ core/src/main/java/com/hushkisses/spacesurvival/facility/action/FacilityActionAccessDecision.java
@@ -0,0 +1,18 @@
+package com.hushkisses.spacesurvival.facility.action;
+
+public record FacilityActionAccessDecision(boolean allowed, DenialReason denialReason) {
+    public static FacilityActionAccessDecision allow() {
+        return new FacilityActionAccessDecision(true, null);
+    }
+
+    public static FacilityActionAccessDecision deny(DenialReason reason) {
+        return new FacilityActionAccessDecision(false, reason);
+    }
+
+    public enum DenialReason {
+        FACILITY_OFFLINE,
+        FACILITY_QUARANTINED,
+        FACILITY_DAMAGED_ADVANCED_UNAVAILABLE,
+        MISSING_ROLE_CAPABILITY
+    }
+}
