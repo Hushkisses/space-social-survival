aa9d92358aac0684b6d87de8524376bb2f6a6c49
Hushkisses <97074606+Hushkisses@users.noreply.github.com>
2026-09-25 15:36:29 +0900

    DEV-012 implement role candidate selection

---
+++ core/src/main/java/com/hushkisses/spacesurvival/role/selection/RoleSelectionResult.java
@@ -0,0 +1,9 @@
+package com.hushkisses.spacesurvival.role.selection;
+
+public enum RoleSelectionResult {
+    SELECTED,
+    CANDIDATES_NOT_PREPARED,
+    ROLE_NOT_OFFERED,
+    ROLE_FULL,
+    ALREADY_SELECTED
+}
