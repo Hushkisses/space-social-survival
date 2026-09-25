97513e41839c9cc921ed3c064148e954cbb37649
Hushkisses <97074606+Hushkisses@users.noreply.github.com>
2026-09-25 14:28:15 +0900

    DEV-004 implement configuration system

---
+++ core/src/main/java/com/hushkisses/spacesurvival/config/InvalidConfigurationException.java
@@ -0,0 +1,8 @@
+package com.hushkisses.spacesurvival.config;
+
+public final class InvalidConfigurationException extends IllegalArgumentException {
+
+    public InvalidConfigurationException(String message) {
+        super(message);
+    }
+}
